e20232ae0e1b5f53843b90929dd90b4652d9bc49
SonamWangyel <stevenrex229@gmail.com>
2024-03-07 15:44:03 +0600

    Modified the generic detail code

--- app/components/GenereicName.js
+++ app/components/GenereicName.js
@@ -1,32 +1,27 @@
 import React, { useState } from 'react';
 
-const MedicineDetails = () => {
+const GenericDetails = () => {
     const [medicineName, setMedicineName] = useState('');
     const [medicineDetails, setMedicineDetails] = useState(null);
     const [error, setError] = useState('');
 
     const fetchMedicineDetails = async () => {
         try {
-            const urlBrand = `https://api.fda.gov/drug/label.json?search=openfda.brand_name:${medicineName}&limit=1`;
             const urlGeneric = `https://api.fda.gov/drug/label.json?search=openfda.generic_name:${medicineName}&limit=1`;
 
-            const responseBrand = await fetch(urlBrand);
             const responseGeneric = await fetch(urlGeneric);
 
-            const dataBrand = await responseBrand.json();
             const dataGeneric = await responseGeneric.json();
 
-            console.log('Brand Medicine details:', dataBrand);
             console.log('Generic Medicine details:', dataGeneric);
 
-            const brandDetails = dataBrand.results.length > 0 ? dataBrand.results[0] : null;
             const genericDetails = dataGeneric.results.length > 0 ? dataGeneric.results[0] : null;
 
-            if (!brandDetails && !genericDetails) {
-                throw new Error(`No medicine found with the name ${medicineName}`);
+            if (!genericDetails) {
+                throw new Error(`No medicine found with the generic name ${medicineName}`);
             }
 
-            setMedicineDetails({ brandDetails, genericDetails });
+            setMedicineDetails(genericDetails);
         } catch (error) {
             setError(error.message);
         }
@@ -57,20 +52,17 @@ const MedicineDetails = () => {
             {error && <p>Error: {error}</p>}
             {medicineDetails && (
                 <div>
-                    <h2 className='px-3'>Medicine Details</h2>
-                    {medicineDetails.brandDetails && (
-                        <>
-                            <p className='px-3'><span className='font-bold'>Brand Name:</span> {medicineDetails.brandDetails.openfda?.brand_name || 'N/A'}</p>                        </>
-                    )}
-                    <p className='px-3'><span className='font-bold'>Indications and Usage:</span> {medicineDetails.brandDetails?.indications_and_usage || medicineDetails.genericDetails?.indications_and_usage || 'N/A'}</p>
-                    <p className='px-3'><span className='font-bold'>Active Ingredient:</span> {medicineDetails.brandDetails?.active_ingredient || medicineDetails.genericDetails?.active_ingredient || 'N/A'}</p>
-                    <p className='px-3'><span className='font-bold'>Directions-1:</span> {medicineDetails.brandDetails?.dosage_and_administration || medicineDetails.genericDetails?.dosage_and_administration || 'N/A'}</p>
-                    <p className='px-3'><span className='font-bold'>Directions-2:</span> {medicineDetails.brandDetails?.do_not_use || medicineDetails.genericDetails?.do_not_use || 'N/A'}</p>
-                    <p className='px-3'><span className='font-bold'>Precaution:</span> {medicineDetails.brandDetails?.keep_out_of_reach_of_children || medicineDetails.genericDetails?.keep_out_of_reach_of_children || 'N/A'}</p>
+                    <h2 className='px-3'>Details</h2>
+                    <p className='px-3'><span className='font-bold'>Brand Name:</span> {medicineDetails.openfda?.brand_name || 'N/A'}</p>
+                    <p className='px-3'><span className='font-bold'>Indications and Usage:</span> {medicineDetails.indications_and_usage || 'N/A'}</p>
+                    <p className='px-3'><span className='font-bold'>Active Ingredient:</span> {medicineDetails.active_ingredient || 'N/A'}</p>
+                    <p className='px-3'><span className='font-bold'>Directions-1:</span> {medicineDetails.dosage_and_administration || 'N/A'}</p>
+                    <p className='px-3'><span className='font-bold'>Directions-2:</span> {medicineDetails.do_not_use || 'N/A'}</p>
+                    <p className='px-3'><span className='font-bold'>Precaution:</span> {medicineDetails.keep_out_of_reach_of_children || 'N/A'}</p>
                 </div>
             )}
         </div>
     );
 };
 
-export default MedicineDetails;
+export default GenericDetails;
--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 "use client"
 import React,{useState} from 'react';
-import MedicineDetails from './components/GenereicName';
+import GenericDetails from './components/GenereicName';
 import Navbar from './components/Navbar';
 
 const Home = () => {
     return (
         <div className='gradient-conic'>
          <Navbar/>
-            <h1 className='py-2 px-3'>Medicine Details </h1>
-            <MedicineDetails />
+            <h1 className='py-2 px-3'>Generic Name Details </h1>
+            <GenericDetails />
         </div>
     );
 };
